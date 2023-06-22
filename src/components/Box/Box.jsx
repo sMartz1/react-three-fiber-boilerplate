c3ea39d2895f404257bbb2ddd996bbda111a79eb
Sebastian Martinez <ra3gan1991@gmail.com>
2023-06-22 17:18:52 +0200

    UseMemo to prevent creating a new instance each time

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,28 +1,32 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect, useMemo } from 'react'
 import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
 export default function Box(props) {
-    const ref = useRef()
-    const [hovered, setHovered] = useState(false)
-    const [rotate, setRotate] = useState(false)
-    useFrame((_, delta) => {
-        if (rotate) {
-            ref.current.rotation.x -= 1 * delta
-            ref.current.rotation.y += 0.5 * delta
-        }
-    })
-    return (
-        <mesh
-            {...props}
-            scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
-            onPointerDown={() => setRotate(!rotate)}
-            onPointerOver={() => setHovered(true)}
-            onPointerOut={() => setHovered(false)}
-            ref={ref}>
-            <boxGeometry />
-            <meshBasicMaterial
-                color={hovered ? 0xff0000 : 0x00ff00}
-                wireframe
-            />
-        </mesh>
-    )
+  const ref = useRef()
+  const [count, setCount] = useState(0)
+  const geometry = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398)],
+    []
+  )
+
+  useEffect(() => {
+    console.log(ref.current.geometry.uuid)
+  })
+
+  useFrame((_, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += 0.5 * delta
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerDown={() => setCount((count + 1) % 2)}
+      geometry={geometry[count]}
+    >
+      <meshBasicMaterial color={'lime'} wireframe />
+    </mesh>
+  )
 }
