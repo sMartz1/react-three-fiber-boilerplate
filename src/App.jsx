52e8125774922ce3eaeec8234de7e6423f360def
Sebastian Martinez <ra3gan1991@gmail.com>
2023-06-24 13:09:27 +0200

    Leva GUI and additional visual helpers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
+import { useMemo } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Box from './components/Box/Box'
 import Polyhedron from './components/Polyhedron/Polyhedron'
 import * as THREE from 'three'
 import { Stats, OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
 export default function App() {
     const polyhedron = [
         new THREE.BoxGeometry(),
         new THREE.SphereGeometry(0.785398),
         new THREE.DodecahedronGeometry(0.785398)
     ]
+
+    const options = useMemo(() => {
+        return {
+            x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+            y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+            z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+            visible: true,
+            color: { value: 'lime' }
+        }
+    }, [])
+
+    const pA = useControls('Polyhedron A', options)
+    const pB = useControls('Polyhedron B', options)
+
     return (
-        <Canvas camera={{ position: [0, 0, 2] }}>
-            {/* <Box position={[-0.75, 0, 0]} name="A" />
-            <Box position={[0.75, 0, 0]} name="B" /> */}
-            <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
-            <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
-            <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
-            <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
-            <OrbitControls />
+        <Canvas camera={{ position: [1, 2, 3] }}>
+            <Polyhedron
+                position={[-1, 1, 0]}
+                rotation={[pA.x, pA.y, pA.z]}
+                visible={pA.visible}
+                color={pA.color}
+                polyhedron={polyhedron}
+            />
+            <Polyhedron
+                position={[1, 1, 0]}
+                rotation={[pB.x, pB.y, pB.z]}
+                visible={pB.visible}
+                color={pB.color}
+                polyhedron={polyhedron}
+            />
+            <OrbitControls target-y={1} />
+            <axesHelper args={[5]} />
+            <gridHelper />
             <Stats />
         </Canvas>
     )
--- src/components/Polyhedron/Polyhedron.jsx
+++ src/components/Polyhedron/Polyhedron.jsx
@@ -1,28 +1,26 @@
-
 import { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 
-export default function Polyhedron({ position, polyhedron }) {
-  const ref = useRef()
-  const [count, setCount] = useState(0)
+export default function Polyhedron({ polyhedron, color, ...props }) {
+    const ref = useRef()
+    const [count, setCount] = useState(0)
 
-  console.log(polyhedron)
+    console.log(polyhedron)
 
-  useFrame((_, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += 0.5 * delta
-  })
+    useFrame((_, delta) => {
+        ref.current.rotation.x += delta
+        ref.current.rotation.y += 0.5 * delta
+    })
 
-  return (
-    <mesh
-      position={position}
-      ref={ref}
-      onPointerDown={() => {
-        setCount((count + 1) % 3)
-      }}
-      geometry={polyhedron[count]}
-    >
-      <meshBasicMaterial color={'lime'} wireframe />
-    </mesh>
-  )
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            onPointerDown={() => {
+                setCount((count + 1) % 3)
+            }}
+            geometry={polyhedron[count]}>
+            <meshBasicMaterial color={color} wireframe />
+        </mesh>
+    )
 }
